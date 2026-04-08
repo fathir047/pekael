ce3dc88d2bd08d55cf0a33909f2a54dc92958e58
fathir047 <zeepreset1@gmail.com>
2026-04-08 14:30:45 +0700

    hosting 2

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
-//siswasmkassalaam_ruangin_qzz_io
-// siswasmkassalaam_ruangin_qzz_io
-// xAxpAzqRkpmQjwkg
-
-
 Route::get('/', [FrontendController::class, 'index']);
 Auth::routes();
 
